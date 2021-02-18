76abb6ecc64a13e05c50136b3d95d4ad53e38061
Ponugoti Siri Theja <psiritheja@gmail.com>
2021-02-18 18:54:48 +0530

    added files

---
+++ shop/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Product, OrderHead, OrderDetail, UserCart
+# Register your models here.
+admin.site.register(Product)
+admin.site.register(OrderHead)
+admin.site.register(OrderDetail)
+admin.site.register(UserCart)
